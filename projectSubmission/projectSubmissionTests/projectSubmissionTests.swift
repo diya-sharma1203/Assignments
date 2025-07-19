5ecc0e661bcb0425eb55d35dabb0f84bc92e9574
diya-sharma1203 <diya0160.be23@chitkara.edu.in>
2025-07-19 16:41:55 +0530

    project

---
+++ projectSubmission/projectSubmissionTests/projectSubmissionTests.swift
@@ -0,0 +1,17 @@
+//
+//  projectSubmissionTests.swift
+//  projectSubmissionTests
+//
+//  Created by Student on 19/07/25.
+//
+
+import Testing
+@testable import projectSubmission
+
+struct projectSubmissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
